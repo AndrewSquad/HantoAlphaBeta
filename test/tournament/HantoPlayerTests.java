4f6c637765d764557c5e38c4e78cd58a5cb66e85
Andrew Botelho <andrewjbotelho@yahoo.com>
2014-10-16 02:23:30 -0400

    writing AI tests 2

--- test/tournament/HantoPlayerTests.java
+++ test/tournament/HantoPlayerTests.java
@@ -11,6 +11,7 @@ import hanto.studentBotelhoLeonard.common.PieceCoordinate;
 import hanto.studentBotelhoLeonard.common.pieces.Butterfly;
 import hanto.studentBotelhoLeonard.common.pieces.Crab;
 import hanto.studentBotelhoLeonard.common.pieces.Horse;
+import hanto.studentBotelhoLeonard.common.pieces.Sparrow;
 import hanto.studentBotelhoLeonard.tournament.HantoPlayer;
 import hanto.tournament.HantoMoveRecord;
 import hanto.common.HantoGameID;
@@ -80,8 +81,68 @@ public class HantoPlayerTests {
 	}
 	
 	@Test
-	public void redNeedsToMakeRandomMove() {
+	public void noLegalJump() {		
+		HantoBoard board = new HantoBoard();
+		board.addPiece(origin, new Horse(BLUE));
+		board.addPiece(new PieceCoordinate(0, 1), new Crab(RED));
+		board.addPiece(new PieceCoordinate(0, -1), new Crab(BLUE));
 		
+		MoveValidator validator = validatorFactory.makeMoveValidator(JUMP, board);
+		assertFalse(validator.existsLegalMove(origin));
+	}
+	
+	@Test
+	public void noLegalFly() {		
+		HantoBoard board = new HantoBoard();
+		board.addPiece(origin, new Sparrow(BLUE));
+		board.addPiece(new PieceCoordinate(0, 1), new Crab(RED));
+		board.addPiece(new PieceCoordinate(0, -1), new Crab(BLUE));
+		
+		MoveValidator validator = validatorFactory.makeMoveValidator(FLY, 4, board);
+		assertFalse(validator.existsLegalMove(origin));
+		
+		assertEquals(0, validator.allMoves(origin).size());
+	}
+	
+	@Test
+	public void optimalFly() {
+		HantoBoard board = new HantoBoard();
+		board.addPiece(origin, new Butterfly(BLUE));
+		board.addPiece(new PieceCoordinate(0, 1), new Crab(RED));
+		board.addPiece(new PieceCoordinate(1, 0), new Butterfly(RED));
+		board.addPiece(new PieceCoordinate(1, -1), new Crab(BLUE));
+		board.addPiece(new PieceCoordinate(-1, 0), new Horse(BLUE));
+		board.addPiece(new PieceCoordinate(-1, 1), new Horse(BLUE));
+		board.addPiece(new PieceCoordinate(0, -1), new Sparrow(BLUE));
+		
+		board.addPiece(new PieceCoordinate(0, 2), new Sparrow(BLUE));
+		board.addPiece(new PieceCoordinate(1, 1), new Crab(RED));
+		board.addPiece(new PieceCoordinate(2, 0), new Crab(RED));
+		board.addPiece(new PieceCoordinate(2, -1), new Crab(BLUE));
+		board.addPiece(new PieceCoordinate(2, -2), new Horse(BLUE));
+		board.addPiece(new PieceCoordinate(1, -2), new Horse(BLUE));
+		board.addPiece(new PieceCoordinate(-1, -1), new Sparrow(BLUE));
+		board.addPiece(new PieceCoordinate(-2, 0), new Crab(RED));
+		board.addPiece(new PieceCoordinate(-2, 1), new Crab(RED));
+		board.addPiece(new PieceCoordinate(-2, 2), new Crab(BLUE));
+		board.addPiece(new PieceCoordinate(-1, 2), new Horse(BLUE));
+		board.addPiece(new PieceCoordinate(0, 3), new Sparrow(BLUE));
+		
+		MoveValidator validator = validatorFactory.makeMoveValidator(FLY, 6, board);
+		PieceCoordinate optimalDest = validator.optimalMove(new PieceCoordinate(0, 3), origin);
+		assertEquals(new PieceCoordinate(0, -2), optimalDest);
+	}
+	
+	@Test
+	public void flyResumeContiguous() {
+		HantoBoard board = new HantoBoard();
+		board.addPiece(origin, new Butterfly(BLUE));
+		board.addPiece(new PieceCoordinate(0, 1), new Crab(RED));
+		board.addPiece(new PieceCoordinate(0, -1), new Sparrow(BLUE));
+		board.addPiece(new PieceCoordinate(1, 0), new Sparrow(BLUE));
+		board.addPiece(new PieceCoordinate(2, -1), new Sparrow(BLUE));
+		MoveValidator validator = validatorFactory.makeMoveValidator(FLY, 4, board);
+		assertTrue(validator.existsLegalMove(new PieceCoordinate(1, 0)));
 	}
 
 }
